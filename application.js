f7230270627eb79bff3eba305ffe564c5068ff68
Trevor Old <trevor.old@outlook.com>
2017-12-30 22:53:29 -0700

    Started adding CSS styling and added username / date of tweet feed

--- application.js
+++ application.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
+function getDate(){
+    var d = new Date();
+    //return d.getFullYear();
+    
+    var month = d.getMonth()+1;
+    var day = d.getDate();
+    var time = d.getHours()+ ':' + d.getMinutes() + ':'+ d.getSeconds();
+    
+    var output = time + ' ' +d.getFullYear() + '/' +
+        (month<10 ? '0' : '') + month + '/' +
+        (day<10 ? '0' : '') + day ;
+        return output;
+}
+
     $('#new-tweet').on('click', 'button', function(){
         var message = $('.tweet-message').val();
-        $('#tweets').append('<div>'+message+'</div>')
-        $('.tweet-message').text();
+        $('#tweets').closest('div')
+                    .prepend('<div><h3>'+message+'</h3><p>@trevorold</p>'+getDate()+'</div>')
+        $('.tweet-message').val('');
     })
 });
