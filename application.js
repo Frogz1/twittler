840f933949f17db9a93acddf98f3b75f0f992a55
Trevor Old <trevor.old@outlook.com>
2017-12-30 22:10:55 -0700

    Implemented add tweet and display tweet

--- application.js
+++ application.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-    
-})
+    $('#new-tweet').on('click', 'button', function(){
+        var message = $('.tweet-message').val();
+        $('#tweets').append('<div>'+message+'</div>')
+        $('.tweet-message').text();
+    })
+});
